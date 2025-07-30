72c71a22bf935b3987bd5c52b4958cd7aae58579
YerayN <ny93yeray@gmail.com>
2025-07-31 00:29:42 +0200

    Archivos base
    Creados archivos base de index, style y script

---
+++ script.js
@@ -0,0 +1,11 @@
+document.getElementById("nuevaPartida").addEventListener("click", () => {
+  alert("Iniciando nueva partida...");
+});
+
+document.getElementById("continuarPartida").addEventListener("click", () => {
+  alert("Cargando partida guardada...");
+});
+
+document.getElementById("campeones").addEventListener("click", () => {
+  alert("Mostrando lista de campeones...");
+});
